7a2717d14eeafa7da4978bfbf74d06b30c09d399
Nikita <nikki-xit@mail.ru>
2023-09-21 20:58:35 +0300

    added a description in the readme about the classes folder. Adding new methods to the playaer class

---
+++ src/utils/classes/player.class.ts
@@ -0,0 +1,62 @@
+import { User } from "./user.class";
+
+export class Player {
+  idUser: User;
+
+  totalMoney: number; // всего денег
+  fieldNumber: number; // на каком номере поля находится
+  moveSkipping: boolean; // пропускает ли ход?
+
+  defeat: boolean;
+  isTurn: boolean; // текущий ли ход игрока?
+  skipNextTurn: boolean; // пропускает ли следующий ход
+
+  constructor(idUser: User) {
+    this.idUser = idUser;
+
+    this.totalMoney = 15000;
+    this.fieldNumber = 0;
+    this.moveSkipping = false;
+    this.defeat = false;
+    this.isTurn = false;
+    this.skipNextTurn = false;
+  }
+
+  // работа с деньгами игрока. Отнимание, плюсовать в зависимости от поля
+  setMoney(amount: number, operator: string = "-"): this {
+    this.totalMoney =
+      operator === "-" ? this.totalMoney - amount : this.totalMoney + amount;
+
+    if (this.totalMoney < 0) {
+      this.totalMoney = 0;
+      this.defeat = true;
+    }
+
+    return this;
+  }
+
+  // установка поля, на котором должен быть игрой (бросается кубик, и подсчитывается, на какое поля игрок попадает)
+  setFieldNumber(amountMove: number[]): this {
+    this.fieldNumber += amountMove.reduce((acc, num) => acc + num, 0);
+
+    if (this.fieldNumber > 40) this.fieldNumber -= 40;
+
+    return this;
+  }
+
+  // конец хода
+  endTurn(): this {
+    this.isTurn = false;
+
+    return this;
+    }
+    
+    skipTurn(): this{
+        this.skipNextTurn = true;
+        this.endTurn();
+
+        
+
+        return this;
+    }
+}
